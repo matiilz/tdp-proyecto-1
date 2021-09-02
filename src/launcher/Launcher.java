cbf3fde842725b0ae666512d3fdf844439131a32
matiilz <matiasilz@outlook.com>
2021-09-02 12:29:03 -0300

    Version final de git y creacion de JAR Ejecutable

--- src/launcher/Launcher.java
+++ src/launcher/Launcher.java
@@ -8,7 +8,7 @@ public class Launcher {
 	
 	public static void main(String[] arg) {
 		
-		Student alumno = new Student(118683,"Ilz","Matias","matyruso10@hotmail.com","https://github.com/matiilz","images/tdp.png");
+		Student alumno = new Student(118683,"Ilz","Matias","matyruso10@hotmail.com","https://github.com/matiilz/tdp-proyecto-1","images/tdp.png");
     	SimplePresentationScreen frame = new SimplePresentationScreen(alumno);
     	frame.setVisible(true);
 		
